cb464f938a2616127ca18de90bdd0f52860dc349
heo <foresty110@gmail.com>
2025-09-16 20:26:42 +0900

    update: 연산 결과값 출력 및 입력 예외처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,16 +9,69 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
 
         //첫 번째 숫자 입력받기
+        int num1 = 0;
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = Integer.parseInt(sc.nextLine());
+
+        try {
+            num1 = Integer.parseInt(sc.nextLine());
+            if (num1 < 0) {
+                System.out.println("양의 정수만 입력할 수 있습니다.");
+            }
+        } catch (NumberFormatException e) {
+            // 숫자가 아닌 값을 입력했을 경우 예외처리
+            System.out.println("숫자를 입력해 주세요.");
+        }
 
 
         //두 번째 숫자 입력받기
+        int num2 = 0;
         System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = Integer.parseInt(sc.nextLine());
+        try {
+            num2 = Integer.parseInt(sc.nextLine());
+            if (num2 < 0) {
+                System.out.println("양의 정수만 입력할 수 있습니다.");
+            }
+        } catch (NumberFormatException e) {
+            // 숫자가 아닌 값을 입력했을 경우 예외처리
+            System.out.println("숫자를 입력해 주세요.");
+        }
 
         // 사칙연산 입력받기
+        char operator ='?';
         System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.nextLine().charAt(0);
+        String input = sc.nextLine();
+        if (input.length() != 1) {
+            System.out.println("기호는 한 개만 입력해 주세요.");
+        }else{
+            operator = input.charAt(0);
+        }
+
+
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = (double) num1 / num2;
+                break;
+            default:
+                System.out.println("지원하지 않는 연산기호입니다. 다시 입력해 주세요.");
+        }
+
+        System.out.println("결과: " + result);
     }
+
 }
+
+
