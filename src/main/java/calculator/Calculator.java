645c767d8585909894c591ab9b07cd05a1254b74
heo <foresty110@gmail.com>
2025-09-16 19:43:55 +0900

    update: 숫자와 사칙연산 기호 입력받기

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,8 +1,24 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+
+        Scanner sc = new Scanner(System.in);
+
+        //첫 번째 숫자 입력받기
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = Integer.parseInt(sc.nextLine());
+
+
+        //두 번째 숫자 입력받기
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = Integer.parseInt(sc.nextLine());
+
+        // 사칙연산 입력받기
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.nextLine().charAt(0);
     }
 }
